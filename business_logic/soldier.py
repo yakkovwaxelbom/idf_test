bd2f02a9d9767f85de89b81b8da2c09d08017363
yakkovwaxelbom <0583281019v@gmail.com>
2025-11-26 15:32:43 +0200

    finish task 1 class

--- business_logic/soldier.py
+++ business_logic/soldier.py
@@ -6,15 +6,11 @@ class Soldier:
         ASSIGNED = 0,
         WAITING = 1
 
-    MASK_PERSONAL_NUMBER = 8 * 10 ** 7
-    SIZE_PERSONAL_NUMBER = 8
-
-    GENDER = ['male', 'female']
+    GENDER = ['male', 'female', 'זכר', 'נקבה']
 
     def __init__(self, personal_number: int, first_name: str, last_name: str, gender: str, city: str
 
                  , distance: float, placement_status: PlacementStatus = PlacementStatus.WAITING):
-
         is_valid, error = Soldier.__validation(personal_number, gender, distance)
 
         if not is_valid:
@@ -30,14 +26,13 @@ class Soldier:
 
     @classmethod
     def __validation(cls, personal_number, gender, distance):
-        if (personal_number & Soldier.MASK_PERSONAL_NUMBER != Soldier.MASK_PERSONAL_NUMBER) and len(
-                str(personal_number)) != Soldier.SIZE_PERSONAL_NUMBER:
+        if personal_number[0] != '8':
             return False, f"personal_number={personal_number}"
 
         if gender not in Soldier.GENDER:
             return False, f"gender={gender}"
 
-        if not isinstance(distance, (int, float)):
+        if not distance.isdigit():
             return False, f"distance={distance}"
         return True, None
 
@@ -59,6 +54,15 @@ class Soldier:
     @property
     def distance(self):
         return self._distance
+
+    @property
+    def placement_status(self):
+        return self._placement_status
+
+    @placement_status.setter
+    def placement_status(self, val):
+        if isinstance(val, Soldier.PlacementStatus):
+            self._placement_status = val
 
     def __str__(self):
         return '\n'.join([f'{k}: {v}' for k, v in self.to_dict().items()])
